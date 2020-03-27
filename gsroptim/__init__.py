fad6816a9ee8fda62a0eb663f9e9a372fc9165bb
mathurinm <mathurin.massias@gmail.com>
2020-03-27 14:25:30 +0100

    [ENH]: test docstring parameters

---
+++ gsroptim/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1'
+
+from .lasso import lasso_path
+from .logreg import logreg_path
+from .multi_task_lasso import multitask_lasso_path
+from .multinomial import multinomial_path
+from .sgl import sgl_path
